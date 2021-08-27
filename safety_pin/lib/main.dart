4f2674727757f461915bd74fa5697713fef8ffd7
Riddhi Dumre <64740500+RiddhiDumre@users.noreply.github.com>
2021-08-27 22:39:58 +0530

    Google Maps added

--- safety_pin/lib/main.dart
+++ safety_pin/lib/main.dart
@@ -2,29 +2,21 @@ import 'package:flutter/material.dart';
 // ignore: unused_import
 import 'package:safety_pin/pages/home.dart';
 import 'package:safety_pin/pages/landingpage.dart';
+
 void main() {
-	runApp(MaterialApp(
-		home: Scroller()
-	));
+  runApp(MaterialApp(home: Scroller()));
 }
 
 class Scroller extends StatelessWidget {
-	const Scroller({ Key? key }) : super(key: key);
-    // static const Color rgbColor=const Color.rgb(216,15,43);
+  const Scroller({Key? key}) : super(key: key);
+  // static const Color rgbColor=const Color.rgb(216,15,43);
 
-	@override
-	Widget build(BuildContext context) {
-		final PageController controller=PageController(initialPage: 0);
-        return PageView(
-            controller: controller,
-            children: [
-                Center(
-                    child: LandingPage()
-                ),
-                Center(
-                    child: Test1()
-                )
-            ],
-        );
-	}
+  @override
+  Widget build(BuildContext context) {
+    final PageController controller = PageController(initialPage: 0);
+    return PageView(
+      controller: controller,
+      children: [Center(child: LandingPage()), Center(child: MyApp())],
+    );
+  }
 }
